357d5bd0e4ed7165592a5e38237338287975617e
LiudmylaSK <liudmyla.s.k.18@gmail.com>
2023-10-29 00:59:25 +0300

    fix App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,6 @@ import { Loader } from "./Loader/Loader";
 import { Modal } from "./Modal/Modal";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-
 Notify.init({
   borderRadius: '10px',
   position: 'left-top',
@@ -23,7 +22,6 @@ Notify.init({
 });
 
 export class App extends Component {
-
   state = {
     query: '',
     images: [],
@@ -34,50 +32,27 @@ export class App extends Component {
     loadMore: false,
     totalPages: 1,
     activeImage: null,
-    isSearchEmpty: false,
   };
 
-  // fetchAllImages = async () => {
-  //   const { query, page } = this.state;
-  //   try {
-  //     this.setState(prevState => ({ isLoading: true }));
-  //     if (!query) return;
+  fetchAllImages = async () => {
+    const { query, page } = this.state;
+    try {
+      this.setState(prevState => ({ isLoading: true }));
+      if (!query) return;
 
-  //     const images = await fetchImages(query, page);
-  //     const pagesCount = Math.ceil(images.totalHits / 12);
-  //     this.setState(prevState => ({
-  //       totalPages: pagesCount,
-  //       images: page === 1 ? images.hits : prevState.images.concat(images.hits),
-  //       loadMore: page < pagesCount,
-  //       isLoading: false,
-  //       isSearchEmpty: false,
-  //     }));
-  //   } catch (error) {
-  //     this.setState({ error: error.message, isLoading: false });
-  //   }
-  // };
-  
- fetchAllImages = async () => {
-  const { query, page, isSearchEmpty } = this.state;
-  try {
-    this.setState(prevState => ({ isLoading: true }));
-    if (!query) return;
+      const images = await fetchImages(query, page);
+      const pagesCount = Math.ceil(images.totalHits / 12);
 
-    const images = await fetchImages(query, page);
-    const pagesCount = Math.ceil(images.totalHits / 12);
-
-    this.setState(prevState => ({
-      totalPages: pagesCount,
-      images: page === 1 ? images.hits : prevState.images.concat(images.hits),
-      loadMore: page < pagesCount,
-      isLoading: false,
-      isSearchEmpty: false,
-    }));
-  } catch (error) {
-    this.setState({ error: error.message, isLoading: false });
-  }
-};
-
+      this.setState(prevState => ({
+        totalPages: pagesCount,
+        images: page === 1 ? images.hits : prevState.images.concat(images.hits),
+        loadMore: page < pagesCount,
+        isLoading: false,
+      }));
+    } catch (error) {
+      this.setState({ error: error.message, isLoading: false });
+    }
+  };
 
   onLoadMore = () => {
     this.setState(prevState => ({
@@ -85,46 +60,42 @@ export class App extends Component {
       page: prevState.page + 1,
     }));
   };
-  
 
-handleSubmit = async event => {
-  event.preventDefault();
-  const search = event.currentTarget.elements.search.value;
-  if (!search.trim()) {
-    this.setState({ isSearchEmpty: true });
-    Notify.info('Please enter a search query');
-  } else {
-    try {
-      this.setState({ query: search, page: 1, isSearchEmpty: false, isLoading: true });
-      const images = await fetchImages(search, 1);
-      const pagesCount = Math.ceil(images.totalHits / 12);
+  handleSubmit = async event => {
+    event.preventDefault();
+    const search = event.currentTarget.elements.search.value;
+    if (!search.trim()) {
+      Notify.info('Please enter a search query');
+    } else {
+      try {
+        this.setState({ query: search, page: 1, isLoading: true });
+        const images = await fetchImages(search, 1);
+        const pagesCount = Math.ceil(images.totalHits / 12);
 
-      if (images.totalHits === 0) {
-        this.setState({ isSearchEmpty: true, isLoading: false });
-        Notify.info('No images found for your query');
-      } else {
-        this.setState(prevState => ({
-          totalPages: pagesCount,
-          images: images.hits,
-          loadMore: 1 < pagesCount,
-          isLoading: false,
-        }));
+        if (images.totalHits === 0) {
+          Notify.info('No images found for your query');
+        } else {
+          this.setState(prevState => ({
+            totalPages: pagesCount,
+            images: images.hits,
+            loadMore: 1 < pagesCount,
+            isLoading: false,
+          }));
+        }
+      } catch (error) {
+        this.setState({ error: error.message, isLoading: false });
       }
-    } catch (error) {
-      this.setState({ error: error.message, isLoading: false });
     }
-  }
+  };
+
+ openModal = selectedImage => {
+  this.setState({ activeImage: selectedImage, isModalOpen: true });
 };
 
-
-  openModal = selectedImage => {
-    this.setState({ activeImage: selectedImage, isModalOpen: true });
-  };
 
   closeModal = () => {
     this.setState({ activeImage: null, isModalOpen: false });
   };
-
 
   componentDidUpdate(_, prevState) {
     const { page, query } = this.state;
@@ -133,14 +104,14 @@ handleSubmit = async event => {
     }
   }
 
-render() {
-    const { isLoading, query, images, loadMore, isModalOpen, activeImage, isSearchEmpty } = this.state;
+  render() {
+    const { isLoading, query, images, loadMore, isModalOpen, activeImage } = this.state;
 
     return (
       <div>
         <Searchbar onSubmit={this.handleSubmit} />
-        {isLoading && !isSearchEmpty && <Loader />}
-        {query && !isSearchEmpty && (
+        {isLoading && <Loader />}
+        {query && (
           <>
             <ImageGallery images={images} onImageClick={this.openModal} />
             {loadMore && <Button onLoadMore={this.onLoadMore} />}
@@ -153,3 +124,4 @@ render() {
     );
   }
 }
+
